9a6c6d15dd4effd8ec990830212481b9926d532e
Zach Stone <hexaphixylix@yahoo.com>
2018-06-07 16:27:36 -0400

    update projects and bio

--- scripts.js
+++ scripts.js
@@ -1,32 +1,27 @@
-let greetingCounter = 1
+let greetingCounter = 1;
 
-const greetings = [
-  "Hello",
-  "こんにちは",
-  "你好",
-  "안녕하세요"
-]
+const greetings = ["Hello", "こんにちは", "你好", "안녕하세요", "Γειά σας"];
 
-const u = "zach"
-const d = "zerovolts"
+const u = "zach";
+const d = "zerovolts";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const greeting = document.getElementsByClassName("greeting")[0]
-  const e = document.getElementById("e")
+  const greeting = document.getElementsByClassName("greeting")[0];
+  const e = document.getElementById("e");
 
-  e.href = "mailto:" + u + "@" + d + ".com"
+  e.href = "mailto:" + u + "@" + d + ".com";
 
   setInterval(() => {
-    greeting.className = "greeting hidden"
+    greeting.className = "greeting hidden";
 
     setTimeout(() => {
-      greeting.className = "greeting"
-      greeting.innerHTML = greetings[greetingCounter]
-      greetingCounter += 1
+      greeting.className = "greeting";
+      greeting.innerHTML = greetings[greetingCounter];
+      greetingCounter += 1;
 
       if (greetingCounter > greetings.length - 1) {
-        greetingCounter -= greetings.length
+        greetingCounter -= greetings.length;
       }
-    }, 500)
-  }, 8000)
-})
+    }, 500);
+  }, 8000);
+});
